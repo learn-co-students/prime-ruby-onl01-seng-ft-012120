30b24c1764282f6d95e026b9ce13c899066e7e0d
Kelly Powers <kpowers.nj@gmail.com>
2020-01-22 22:39:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+
+def prime?(int)
+  array = (2...int).to_a 
+  if int <= 1 
+    return false
+  elsif int == 2 || int == 3 
+    return true 
+  else 
+    array.none? {|i| int % i == 0}
+   end
+end
+
+prime?(4)
