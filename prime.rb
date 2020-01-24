9bbb5909af8333e39cd440371360e33d71560461
reddevilcero <reddevil_cero@hotmail.com>
2020-01-24 00:11:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+require 'pry'
+
+def prime?(num)
+  if num < 2 
+    return false
+  end
+ i = num - 1
+
+ while i > 1
+    if num % i == 0
+      return false
+    end
+    i-=1
+  end
+  true
+end
