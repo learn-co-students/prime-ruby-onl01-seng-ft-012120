c082fd096b527531a6dc77f8ab2a6a11fab0d469
Kyle McKenna <mckennakyler@gmail.com>
2020-01-26 02:29:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1  
+    result = false
+  elsif num == 2 
+    result = true 
+  end
+  array = * (2 ... num)
+  array.each do |a|
+    if num % a == 0 
+      return false 
+    else
+      result = true 
+    end 
+  end
+  return result
+end
+
+prime?(2)
+      
+# After "learn submit" you can "git checkout solution"
+# to see how the school expected it to be solved
+#comment out multiply lines with Ctrl + /
