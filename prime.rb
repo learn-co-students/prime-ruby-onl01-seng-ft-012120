08d91e5551e8abb608de21d97d31cda704765231
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-26 13:55:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
+require "pry"
+
 # Add  code here!
+def prime?(number)
+  
+  if number < 0 || number ==1 || number == 0 
+    
+    return false
+    
+  else
+    
+    (2...number).to_a.all? do |num|
+      number %  num != 0 
+      
+      binding.pry
+    end 
+  end
+end
