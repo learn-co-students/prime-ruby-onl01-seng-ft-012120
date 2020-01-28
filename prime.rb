5372fc314c8e1f22cc2637e71166b95dd67a304a
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-28 21:49:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  fn = 2
+  if number > 1
+    all_n = (fn..number-1).to_a
+    all_n.none? do |num_test|
+      number % num_test == 0
+    end
+  else
+    false
+  end
+end
